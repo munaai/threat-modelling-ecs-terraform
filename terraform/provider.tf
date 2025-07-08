d432aa49920c5245a80a3eb6e7eb0e3b11ea229d
munaibrahimn <munaa97@gmail.com>
2025-07-08 14:37:01 +0100

    tf vars stored in s3

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -8,11 +8,18 @@ terraform {
   }
 }
 resource "aws_s3_bucket" "tfvars_config" {
-  bucket = "my-terraform-config-bucket"
-
+  bucket = "my-terraform-config-bucket-muna"
+  force_destroy = true
   tags = {
     Name = "Terraform tfvars bucket"
   }
+}
+resource "aws_s3_bucket_public_access_block" "tfvars_config_block" {
+  bucket = aws_s3_bucket.tfvars_config.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 # Provider block - Tells terraform which cloud provider to interract with
 provider "aws" {
