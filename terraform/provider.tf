340fbe8a4c57ce63ea859640dcf1c86ad9d7ea16
munaibrahimn <munaa97@gmail.com>
2025-07-08 14:18:50 +0100

    setting up pipeline

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -7,7 +7,13 @@ terraform {
     }
   }
 }
+resource "aws_s3_bucket" "tfvars_config" {
+  bucket = "my-terraform-config-bucket"
 
+  tags = {
+    Name = "Terraform tfvars bucket"
+  }
+}
 # Provider block - Tells terraform which cloud provider to interract with
 provider "aws" {
   # Configuration options
