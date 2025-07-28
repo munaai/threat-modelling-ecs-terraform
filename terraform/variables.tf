3cac13f06643efd0f970ec19a99492ab37559b0d
munaibrahimn <munaa97@gmail.com>
2025-07-28 15:13:26 +0100

    tflint update

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,4 @@
+# ECS Fargate
 variable "cluster_name" {
   description = "Name of the ECS cluster"
   type        = string
@@ -14,61 +15,66 @@ variable "desired_count" {
 }
 
 variable "cluster_insight_name" {
-  description = "name of the cluster setting name"
+  description = "Name of the ECS cluster setting"
   type        = string
 }
 
 variable "cluster_insight_value" {
-  description = "name of the cluster setting value"
+  description = "Value of the ECS cluster setting"
   type        = string
 }
 
 variable "container_name" {
-  description = "name of the container"
+  description = "Name of the container"
   type        = string
 }
 
 variable "container_port" {
-  description = "container port"
+  description = "Container port"
   type        = number
 }
 
 variable "task_family" {
-  description = "family name"
+  description = "Family name for ECS task definition"
   type        = string
 }
 
 variable "task_cpu" {
-  description = "cpu"
+  description = "CPU units for the task"
   type        = string
 }
 
 variable "task_memory" {
-  description = "memory"
+  description = "Memory for the task"
   type        = string
 }
 
 variable "image_url" {
-  description = "link of the image in ecr"
+  description = "Image URL in ECR"
   type        = string
 }
 
+# IAM
 variable "role_name" {
   description = "IAM role name"
   type        = string
 }
 
 variable "assume_service" {
-  type = string
+  description = "Service to assume the IAM role"
+  type        = string
 }
 
 variable "policy_name" {
-  type = string
+  description = "IAM policy name"
+  type        = string
 }
 
+# Security Groups
 variable "alb_sg_name" {
   type = string
 }
+
 variable "alb_sg_description" {
   type = string
 }
@@ -76,6 +82,7 @@ variable "alb_sg_description" {
 variable "alb_ingress_http_from_port" {
   type = number
 }
+
 variable "alb_ingress_http_to_port" {
   type = number
 }
@@ -88,6 +95,7 @@ variable "alb_ingress_cidr_blocks" {
 variable "alb_ingress_https_from_port" {
   type = number
 }
+
 variable "alb_ingress_https_to_port" {
   type = number
 }
@@ -100,89 +108,137 @@ variable "ecs_egress_cidr_blocks" {
 variable "ecs_sg_name" {
   type = string
 }
+
 variable "ecs_sg_description" {
   type = string
 }
+
 variable "ecs_ingress_from_port" {
   type = number
 }
+
 variable "ecs_ingress_to_port" {
   type = number
 }
+
 variable "ecs_egress_from_port" {
   type = number
 }
+
 variable "ecs_egress_to_port" {
   type = number
 }
+
 variable "ingress_protocol" {
   type = string
 }
+
 variable "egress_protocol" {
   type = string
 }
-variable "alb_name" {}
+
+# ALB / ELB
+variable "alb_name" {
+  type = string
+}
+
 variable "alb_internal" {
   type    = bool
   default = false
 }
+
 variable "alb_deletion_protection" {
   type    = bool
   default = false
 }
 
-variable "target_group_name" {}
+variable "target_group_name" {
+  type = string
+}
+
 variable "target_group_protocol" {
+  type    = string
   default = "HTTP"
 }
 
+# Health Check
 variable "health_check_path" {
+  type    = string
   default = "/"
 }
+
 variable "health_check_interval" {
+  type    = number
   default = 30
 }
+
 variable "health_check_timeout" {
+  type    = number
   default = 5
 }
+
 variable "health_check_healthy_threshold" {
+  type    = number
   default = 2
 }
+
 variable "health_check_unhealthy_threshold" {
+  type    = number
   default = 2
 }
+
 variable "health_check_matcher" {
+  type    = string
   default = "200"
 }
 
+# Listener
 variable "listener_port" {
+  type    = number
   default = 80
 }
+
 variable "listener_protocol" {
+  type    = string
   default = "HTTP"
 }
-variable "hosted_zone_id" {}
 
-variable "record_name" {}
+# Route 53
+variable "hosted_zone_id" {
+  type = string
+}
+
+variable "record_name" {
+  type = string
+}
+
 variable "record_type" {
+  type    = string
   default = "A"
 }
+
+# ACM / TLS
 variable "certificate_arn" {
   description = "ARN of the ACM certificate"
   type        = string
 }
 
 variable "ssl_policy" {
+  type    = string
   default = "ELBSecurityPolicy-2016-08"
 }
+
 variable "https_listener_port" {
+  type    = number
   default = 443
 }
 
 variable "https_listener_protocol" {
+  type    = string
   default = "HTTPS"
 }
 
+# VPC & Subnets
 variable "vpc_cidr_block" {
   description = "CIDR block for the VPC"
   type        = string
@@ -198,11 +254,13 @@ variable "azs" {
   type        = list(string)
 }
 
+# Tags
 variable "tags" {
   description = "Tags to apply to all resources"
   type        = map(string)
 }
 
+# HTTP → HTTPS Redirect
 variable "http_listener_port" {
   type        = number
   description = "Port for HTTP listener"
@@ -217,9 +275,3 @@ variable "http_redirect_status_code" {
   type        = string
   description = "Redirect status code (e.g., HTTP_301)"
 }
-
-variable "https_listener_port" {
-  type        = number
-  description = "Port for HTTPS listener (used in redirect)"
-}
-
